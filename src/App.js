f0d72c306e2846f5e716525a8cb1f4041da7bc3d
olawanlejoel <info4tobest2@gmail.com>
2022-02-16 11:53:50 +0000

    first push

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from 'react';
+
+import { IoIosCheckmarkCircle } from 'react-icons/io';
+import { MdDeleteForever } from 'react-icons/md';
+import { FiCircle } from 'react-icons/fi';
+
+const App = () => {
+	const [todos, setTodos] = useState([]);
+	const [todoItem, setTodoItem] = useState('');
+	const [error, setError] = useState(false);
+	const [completedTasks, setCompletedTasks] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (todoItem) {
+			setError(false);
+			let uniqueId =
+				new Date().getTime().toString(36) + new Date().getUTCMilliseconds();
+			let newTodoItem = {
+				id: uniqueId,
+				todo: todoItem,
+				complete: false,
+			};
+			setTodos([newTodoItem, ...todos]);
+			setTodoItem('');
+		} else {
+			setError(true);
+			setTodoItem('');
+		}
+	};
+
+	const deleteTodo = (id) => {
+		let newTodos = todos.filter((todo) => todo.id !== id);
+		setTodos([...newTodos]);
+	};
+
+	const toggleComplete = (id) => {
+		todos.find((todo) => {
+			if (todo.id === id) {
+				todo.complete = !todo.complete;
+			}
+			setTodos([...todos]);
+		});
+	};
+
+	useEffect(() => {
+		let completeArray = [];
+		todos.filter((todo) => {
+			if (todo.complete === true) {
+				completeArray.push(todo);
+			}
+		});
+		setCompletedTasks(completeArray.length);
+	}, [todos]);
+
+	useEffect(() => {
+		const todos = JSON.parse(localStorage.getItem('todos'));
+		if (todos) {
+			setTodos(todos);
+		}
+	}, []);
+
+	useEffect(() => {
+		let adderror = setTimeout(() => {
+			setError(false);
+		}, 2000);
+		return () => {
+			clearTimeout(adderror);
+		};
+	}, [error]);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
+	let Today = new Date().toLocaleDateString('en-us', { weekday: 'long' });
+	let day = new Date().toLocaleDateString('en-us', { day: 'numeric' });
+	let month = new Date().toLocaleDateString('en-us', { month: 'short' });
+
+	return (
+		<div className="app-container">
+			<div className="header-section">
+				<h4 className="date">
+					{`${Today},`} <span>{`${day} ${month}`}</span>
+				</h4>
+				<div className="app-form-container">
+					<form onSubmit={handleSubmit}>
+						<input
+							type="text"
+							value={todoItem}
+							className={error ? 'error' : ''}
+							onChange={(e) => setTodoItem(e.target.value)}
+							placeholder="Type Todo here..."
+						/>
+						<button type="submit" className="btn">
+							Add Todo
+						</button>
+					</form>
+				</div>
+				<div className="data-card-container">
+					<div className="data-card">
+						<h5>{todos.length < 10 ? `0${todos.length}` : todos.length}</h5>
+						<p>Created tasks</p>
+					</div>
+					<div className="data-card">
+						<h5>
+							{completedTasks < 10 ? `0${completedTasks}` : completedTasks}
+						</h5>
+						<p>Completed tasks</p>
+					</div>
+				</div>
+			</div>
+			<div className="todo-container">
+				{todos.map((todoItem) => {
+					const { id, todo, complete } = todoItem;
+					return (
+						<div key={id} className="todo-card">
+							<div className="icon" onClick={() => toggleComplete(id)}>
+								{!complete ? (
+									<FiCircle />
+								) : (
+									<IoIosCheckmarkCircle
+										className={complete ? 'icon-done' : ''}
+									/>
+								)}
+							</div>
+							<p className={complete ? 'text-done' : ''}>{todo}</p>
+							<MdDeleteForever
+								onClick={() => deleteTodo(id)}
+								className="icon delete-icon"
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default App;
